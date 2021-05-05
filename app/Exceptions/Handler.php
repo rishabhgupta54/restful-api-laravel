f7f922c3cec32f85e44d578d064a04637803dcb2
Rishabh <programmer.rishabh@gmail.com>
2021-05-05 23:42:20 +0530

    Errors & Exceptions Handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,10 +2,25 @@
 
 namespace App\Exceptions;
 
+use App\Traits\APIResponser;
+use function class_basename;
+use function dd;
+use HttpException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Events\Authenticated;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use function response;
+use function strtolower;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 use Throwable;
 
 class Handler extends ExceptionHandler {
+    use APIResponser;
     /**
      * A list of the exception types that are not reported.
      * @var array
@@ -31,5 +46,41 @@ class Handler extends ExceptionHandler {
         $this->reportable(function(Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e) {
+        if ($e instanceof ValidationException) {
+            return $this->convertValidationExceptionToResponse($e, $request);
+        }
+
+        if ($e instanceof ModelNotFoundException) {
+            return $this->errorResponse(class_basename($e->getModel()) . ' not found', 404);
+        }
+
+        if ($e instanceof AuthenticationException) {
+            return $this->errorResponse($request, 403);
+        }
+
+        if ($e instanceof AuthorizationException) {
+            return $this->errorResponse($e->getMessage(), 403);
+        }
+
+        if ($e instanceof NotFoundHttpException) {
+            return $this->errorResponse('The specific URL cannot be found', 404);
+        }
+
+        if ($e instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse('The specified request for this request if not allowed', 405);
+        }
+
+        if ($e instanceof HttpException) {
+            return $this->errorResponse($e->getMessage(), $e->getCode());
+        }
+
+        return $this->errorResponse('Something went wrong, please try again.', $e->getCode());
+    }
+
+    public function convertValidationExceptionToResponse(ValidationException $e, $request) {
+        return $this->errorResponse($e->validator->errors()->getMessages(), 422);
     }
 }

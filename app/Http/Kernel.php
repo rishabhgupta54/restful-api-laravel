da147fdacd0aca179708865ff72a986ccbeb5632
Rishabh Gupta <programmer.rishabh@gmail.com>
2021-05-09 17:57:44 +0530

    Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends HttpKernel {
      */
     protected $middlewareGroups = [
         'web' => [
+            'signature:X-Application-Name',
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
@@ -36,6 +37,7 @@ class Kernel extends HttpKernel {
         ],
 
         'api' => [
+            'signature:X-Application-Name',
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
@@ -56,5 +58,6 @@ class Kernel extends HttpKernel {
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'signature' => \App\Http\Middleware\SignatureMiddleware::class,
     ];
 }
